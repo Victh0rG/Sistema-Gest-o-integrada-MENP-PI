919ca8eee2be2c2905f0a483f96ac5b9857caef4
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2026-03-10 12:09:38 -0300

    feat: criando pastas utilizando o metodo files

--- infraestructure/drive_repository.py
+++ infraestructure/drive_repository.py
@@ -1,1 +1,24 @@
 # Upload / Download Google Drive
+import google_auth
+
+#
+# class DriveRepository():
+#     def __init__(self):
+#         self.google_auth = google_auth.GoogleAuth()
+
+auth = google_auth.GoogleAuthManager()
+if auth.login():
+    drive = auth.get_drive_service()
+
+    paths = ['sead']
+    drive.files().create(
+        body={
+            'name' : paths[0],
+            'mimeType' : 'application/vnd.google-apps.folder',
+        }
+    ).execute()
+
+
+
+
+
