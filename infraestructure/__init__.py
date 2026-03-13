a03d4c192337d6b0ab6c24132c84ad818892556a
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2026-03-13 11:59:21 -0300

    feat: class para authenticar, evitando código duplicado ou excesso de autenticações

--- infraestructure/__init__.py
+++ infraestructure/__init__.py
@@ -1,7 +1,23 @@
-import google_auth
 
-auth = google_auth.GoogleAuthManager()
-if auth.login():
-    sheets = auth.get_sheets_service()
-    drive = auth.get_drive_service()
-    user = auth.get_user_info()
+from . import google_auth
+
+class Authenticate:
+    """ Instancia da classe GoogleAuthManager para ouath2"""
+
+    _instance = None
+
+    def __init__(self):
+        self.auth = google_auth.GoogleAuthManager()
+        self.services = None
+
+    def authenticate(self):
+
+        if not self.services:
+            if self.auth.login():
+                self.services = {
+                    "sheets": self.auth.get_sheets_service(),
+                    "drive": self.auth.get_drive_service(),
+                    "user": self.auth.get_user_info()
+                }
+
+        return self.services
