7276acb607503df79e4229fdae029438d20941fa
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2026-03-16 17:54:46 -0300

    feat: add serviço de leitura de arquivos e criação de imagem

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -1,1 +1,21 @@
-# Extração de texto PDF (pdfplumber)
+# Extração de texto PDF
+from pypdf import PdfReader
+
+def pdf_has_text(pdf_file):
+    reader = PdfReader(pdf_file)
+    page = reader.pages[0]
+    text = page.extract_text()
+    print(text)
+    return text
+    # for page in reader.pages:
+    #     text = page.extract_text()
+    #     if text and text.strip():
+    #         return True
+    #     return False
+
+def pdf_has_image(pdf_file):
+    reader = PdfReader(pdf_file)
+    page = reader.pages[0]
+    for i, image_file_object in enumerate(page.images):
+        file_name = "out-image-" + str(i) + "-" + image_file_object.name
+        image_file_object.image.save(file_name)
